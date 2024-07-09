7746bdaca2c10f42298ca1ef3e60c7d61ac6fe4f
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-09 04:36:05 +0000

    Set up the testing environment for the databases

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- This script sets up database testing environment with a database and a user with privileges.
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
