a2507a946a2e4a9f78709c8919163e3d2906c87c
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-14 04:25:56 +0000

    Create an archive from the contents of a local file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+import os
+from fabric.api import local, run
+from datetime import datetime
+
+
+def do_pack():
+    '''
+    do_pack function creates an archive
+    from the contents of a directory
+    '''
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_{}.{}".format(time, "tgz")
+
+    if not os.path.exists("versions"):
+        local('mkdir versions')
+        
+    status = local('tar -cvzf {} web_static'.format(archive_name))
+    if status.succeeded and os.path.exists(archive_name):
+        return archive_name
+    return None
