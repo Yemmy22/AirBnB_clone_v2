1f30f03eadc761fa8efe0a8b2a558f9e6bab15f2
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-20 07:09:55 +0000

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ from fabric.api import local, run, env, put
 env.user = 'ubuntu'
 env.hosts = ['100.26.138.201', '54.209.126.230']
 env.key_filename = "~/.ssh/id_rsa"
+file_name = "web_static"
 
 
 def do_deploy(archive_path):
@@ -17,7 +18,8 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    time_stamp = archive_path[-18:-4]
+    parent_dir, archive_name = archive_path.split('/')
+    archive_dir, ext = archive_name.split('.')
 
     try:
         # Upload archive to the /tmp/ directory of remote
@@ -25,39 +27,27 @@ def do_deploy(archive_path):
 
         # Create a new directory with the archive name in the
         # /data/web_static/releases
-        run('mkdir -p\
-                /data/web_static/releases/web_static_{}'.format(time_stamp))
+        run("mkdir -p  /data/web_static/releases/{}/".format(archive_dir))
 
         # Uncompress the archive into the newly created directory
-        run('tar -xzf\
-                /tmp/web_static_{}.tgz -C\
-                /data/web_static/releases/web_static_{}/'.format(
-                    time_stamp, time_stamp))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_name, archive_dir))
 
         # Deletes the archive from the tmp directory
-        run('rm /tmp/web_static_{}.tgz'.format(time_stamp))
+        run("rm /tmp/{}".format(archive_name))
 
         # Move the content of the uncompressed archive
         # into its grand-parent directory
-        run('rsync -aI\
-                /data/web_static/releases/web_static_{}/web_static/*\
-                /data/web_static/releases/web_static_{}/'.format(
-                    time_stamp, time_stamp))
+        run('rsync -aI /data/web_static/releases/{}/{}/* /data/web_static/releases/{}/'.format(archive_dir, file_name, archive_dir))
 
         # Delete the empty archive directory
-        run('rm -rf\
-                /data/web_static/releases/web_static_{}/web_static'.format(
-                    time_stamp))
+        run('rm -rf /data/web_static/releases/{}/{}'.format(archive_dir, file_name))
 
         # Deletes the symbolic link /data/web_static/current
         # from the web server
-        run('rm -rf /data/web_static/current')
+        run("rm -rf /data/web_static/current")
 
         # Recreates a new the symbolic link
-        run('ln -s\
-                /data/web_static/releases/web_static_{}/\
-                /data/web_static/current'.format(
-                    time_stamp))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_dir))
 
         # Reload the server
         run('sudo service nginx restart')
