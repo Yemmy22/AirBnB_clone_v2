2003ba9bea53ceba8f5fb7a1748df5c117569cd7
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-20 06:56:42 +0000

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,27 +25,39 @@ def do_deploy(archive_path):
 
         # Create a new directory with the archive name in the
         # /data/web_static/releases
-        run('mkdir -p  /data/web_static/releases/web_static_{}'.format(time_stamp))
+        run('mkdir -p\
+                /data/web_static/releases/web_static_{}'.format(time_stamp))
 
         # Uncompress the archive into the newly created directory
-        run('tar -xzf /tmp/web_static_{}.tgz -C /data/web_static/releases/web_static_{}/'.format(time_stamp, time_stamp))
+        run('tar -xzf\
+                /tmp/web_static_{}.tgz -C\
+                /data/web_static/releases/web_static_{}/'.format(
+                    time_stamp, time_stamp))
 
         # Deletes the archive from the tmp directory
         run('rm /tmp/web_static_{}.tgz'.format(time_stamp))
 
         # Move the content of the uncompressed archive
         # into its grand-parent directory
-        run('rsync -aI /data/web_static/releases/web_static_{}/web_static/* /data/web_static/releases/web_static_{}/'.format(time_stamp, time_stamp))
+        run('rsync -aI\
+                /data/web_static/releases/web_static_{}/web_static/*\
+                /data/web_static/releases/web_static_{}/'.format(
+                    time_stamp, time_stamp))
 
         # Delete the empty archive directory
-        run('rm -rf /data/web_static/releases/web_static_{}/web_static'.format(time_stamp))
+        run('rm -rf\
+                /data/web_static/releases/web_static_{}/web_static'.format(
+                    time_stamp))
 
         # Deletes the symbolic link /data/web_static/current
         # from the web server
         run('rm -rf /data/web_static/current')
 
         # Recreates a new the symbolic link
-        run('ln -s /data/web_static/releases/web_static_{}/ /data/web_static/current'.format(time_stamp))
+        run('ln -s\
+                /data/web_static/releases/web_static_{}/\
+                /data/web_static/current'.format(
+                    time_stamp))
 
         # Reload the server
         run('sudo service nginx restart')
