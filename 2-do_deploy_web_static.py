fe5750ccdf2ab18e3bf59b53fe9809fa06c23aa9
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-20 07:21:11 +0000

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,24 +30,33 @@ def do_deploy(archive_path):
         run("mkdir -p  /data/web_static/releases/{}/".format(archive_dir))
 
         # Uncompress the archive into the newly created directory
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_name, archive_dir))
+        run("tar -xzf\
+                /tmp/{} -C /data/web_static/releases/{}/".format(
+                    archive_name, archive_dir))
 
         # Deletes the archive from the tmp directory
         run("rm /tmp/{}".format(archive_name))
 
         # Move the content of the uncompressed archive
         # into its grand-parent directory
-        run('rsync -aI /data/web_static/releases/{}/{}/* /data/web_static/releases/{}/'.format(archive_dir, file_name, archive_dir))
+        run('rsync -aI\
+                /data/web_static/releases/{}/{}/*\
+                /data/web_static/releases/{}/'.format(
+                    archive_dir, file_name, archive_dir))
 
         # Delete the empty archive directory
-        run('rm -rf /data/web_static/releases/{}/{}'.format(archive_dir, file_name))
+        run('rm -rf /data/web_static/releases/{}/{}'.format(
+            archive_dir, file_name))
 
         # Deletes the symbolic link /data/web_static/current
         # from the web server
         run("rm -rf /data/web_static/current")
 
         # Recreates a new the symbolic link
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_dir))
+        run("ln -s\
+                /data/web_static/releases/{}/\
+                /data/web_static/current".format(
+                    archive_dir))
 
         # Reload the server
         run('sudo service nginx restart')
