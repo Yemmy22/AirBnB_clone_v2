5b26787f46cd5f5cade55fae70b3939a325c259a
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-08-06 15:22:45 +0000

    Add number_bathrooms class attribute to place class

--- models/place.py
+++ models/place.py
@@ -29,12 +29,13 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ ='places'
+        __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60),  ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024))
         number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, default=0)
@@ -48,5 +49,3 @@ class Place(BaseModel, Base):
         Initializes instances of a Place.
         '''
         super().__init__(*args, **kwargs)
-
-   
